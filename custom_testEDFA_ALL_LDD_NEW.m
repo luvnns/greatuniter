c24008a72c9e176f4460f6131d2319f5a432efd6
agodovova <anastasiagodovova@ya.ru>
2022-08-02 14:33:27 +0300

    addStr and LDDwarning was added to custom script

--- custom_testEDFA_ALL_LDD_NEW.m
+++ custom_testEDFA_ALL_LDD_NEW.m
@@ -1,12 +1,12 @@
 %% Initial parameters
 sampleTablePath = 'C:\greatuniter_test2807\tables\testEDFA_ALL_LDD.xlsx';
-userText = 'second_script_test';
+userText = 'test';
 referenceTrace = 'A';
 waveformTrace = 'B';
-rowNumberToWriteStart = 4;
-rowNumberToWriteStop = 7;
-rowNumberToReadStart = 13;
-rowNumberToReadStop = 27;
+varNumberToWriteStart = 4;
+varNumberToWriteStop = 7;
+varNumberToReadStart = 13;
+varNumberToReadStop = 27;
 
 %% Copy windows from the Great Uniter
 OPMrubin_Power_input = mainApp.struct_OPMrubin.Power_input;
@@ -15,19 +15,33 @@ FPGA = mainApp.FPGA;
 ATTENniMyDaq = mainApp.ATTENniMyDaq;
 SWITCHosaEdfa = mainApp.SWITCHosaEdfa;
 %% Get table from file
-sampleTable = readtable(sampleTablePath);%'ReadRowNames',false,'ReadVariableNames',true
+sampleTable = readtable(sampleTablePath);
 
 %% Cycle for table rows with showing in process(?)
+refreshProgress(mainApp.mainWindow,0);
 tablePath = [OSAyokogawa.folder,filesep,userText,createFilename(now,'result_'),'.xlsx'];
+addStrOutput(mainApp.mainWindow,[ ...
+    'There was created table: ', ...
+    tablePath, ...
+    ]);
+addStrOutput(mainApp.mainWindow,'');
+addStrOutput(mainApp.mainWindow,'DO NOT OPEN TABLE UNTIL FINISHED');
+addStrOutput(mainApp.mainWindow,'Use opened figure instead');
+addStrOutput(mainApp.mainWindow,'');
 ATTENniMyDaq = setAttenuation(ATTENniMyDaq,0);
 FPGA = writeData(FPGA, FPGA.FLASH_MEM, 'MODE', '0');
+addStrOutput(mainApp.mainWindow,'Write to FPGA MODE = 0');
+enableWarningLaser(mainApp.mainWindow);
 FPGA = writeData(FPGA, FPGA.FLASH_MEM, 'LDD_EN', 'f');
+addStrOutput(mainApp.mainWindow,'Write to FPGA LDD_EN = F');
 varNames = sampleTable.Properties.VariableNames;
 height_sampleTable = height(sampleTable);
 statusTable = StatusTable(sampleTable);
 for rowNumber = 1:height_sampleTable
     SWITCHosaEdfa = switchSignalTo(SWITCHosaEdfa,'OSA');
+    addStrOutput(mainApp.mainWindow,SWITCHosaEdfa.infoString);
     powerTh = sampleTable{rowNumber,'InputPowerThDBm'};
+    addStrOutput(mainApp.mainWindow,'Calibrate attenuation...');
     counter = 0;
     counterStop = 6;
     OPMrubin_Power_input = requestPower(OPMrubin_Power_input);
@@ -42,20 +56,31 @@ for rowNumber = 1:height_sampleTable
         difference = powerReal - powerTh;
     end
     sampleTable{rowNumber,'InputPowerRealDBm'} = powerReal;
-    pause(sampleTable{rowNumber,'PauseAfterAttenuationS'});
+    addStrOutput(mainApp.mainWindow,['Attenuation is ', attenuation]);
+    pauseAfterAttenuation = sampleTable{rowNumber,'PauseAfterAttenuationS'};
+    addStrOutput(mainApp.mainWindow,['Pause for ',num2str(pauseAfterAttenuation),' s']);
+    pause(pauseAfterAttenuation);
+    addStrOutput(mainApp.mainWindow,['Write reference trace ',referenceTrace]);
     writeWaveform(OSAyokogawa,referenceTrace);
     pause(5);
     SWITCHosaEdfa = switchSignalTo(SWITCHosaEdfa,'EDFA');
-    for j = rowNumberToWriteStart:rowNumberToWriteStop
+    addStrOutput(mainApp.mainWindow,SWITCHosaEdfa.infoString);
+    for j = varNumberToWriteStart:varNumberToWriteStop
         name = varNames{j};
         data = string(sampleTable{rowNumber,name});
         FPGA = writeData(FPGA, FPGA.FLASH_MEM, name, data);
+        addStrOutput(mainApp.mainWindow,['Write to FPGA ',name,' = ',data]);
     end
-
-    pause(sampleTable{rowNumber,'PauseAfterSetOutputPowerS'})
+    pauseAfterSetOutputPower = sampleTable{rowNumber,'PauseAfterSetOutputPowerS'};
+    addStrOutput(mainApp.mainWindow,['Pause for ',num2str(pauseAfterSetOutputPower),' s']);
+    pause(pauseAfterSetOutputPower)
+    addStrOutput(mainApp.mainWindow,['Write waveform trace ',waveformTrace]);
     writeWaveform(OSAyokogawa,waveformTrace);
+    addStrOutput(mainApp.mainWindow,['Read waveform trace ',waveformTrace]);
     OSAyokogawa = readWaveform(OSAyokogawa,waveformTrace);
+    addStrOutput(mainApp.mainWindow,['Save waveform trace ',waveformTrace]);
     saveWaveform(OSAyokogawa,userText);
+    addStrOutput(mainApp.mainWindow,'Read waveform parameters');
     sampleTable{rowNumber,'OutputPowerRealDBmOSA'} = readPower(OSAyokogawa,waveformTrace);
     OSAyokogawa = readAnalysisEDFANF(OSAyokogawa,waveformTrace);
     saveAnalysisEDFANF(OSAyokogawa,userText);
@@ -63,21 +88,23 @@ for rowNumber = 1:height_sampleTable
     sampleTable{rowNumber,'MaxNF'} = max(edfa_nf_table{:,'nf'});
     sampleTable{rowNumber,'MeanGAIN'} = mean(edfa_nf_table{:,'gain'});
     sampleTable{rowNumber,'DeltaGAIN'} = max(edfa_nf_table{:,'gain'})-min(edfa_nf_table{:,'gain'});
-    for z = rowNumberToReadStart:rowNumberToReadStop
+    addStrOutput(mainApp.mainWindow,'Read FPGA parameters');
+    for z = varNumberToReadStart:varNumberToReadStop
         name = varNames{z};
         FPGA = readData(FPGA, FPGA.FLASH_MEM, name);
         sampleTable{rowNumber,name} = str2double(FPGA.lastRead);
     end
-    
+    addStrOutput(mainApp.mainWindow,'Update result table');
     writetable(sampleTable,tablePath);
     if ~isvalidFigure(statusTable)
         statusTable = StatusTable(sampleTable);
     else
         statusTable = refreshTable(statusTable,sampleTable);
     end
-    clc
-    %disp(sampleTable)
-    fprintf('%0.0f %%\n', rowNumber/height_sampleTable*100)
+    progress = rowNumber / height_sampleTable * 100;
+    refreshProgress(mainApp.mainWindow,progress);
 end
 FPGA = writeData(FPGA, FPGA.FLASH_MEM, 'LDD_EN', '0');
-disp('finished')
+addStrOutput(mainApp.mainWindow,'Write to FPGA LDD_EN = 0');
+disableWarningLaser(mainApp.mainWindow);
+addStrOutput(mainApp.mainWindow,'Custom script finished');
